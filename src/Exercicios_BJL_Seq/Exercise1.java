cf5e53a935f621127434c4ebfae52e3ce870f3a3
Matheus Fillipe <mfsm@cin.ufpe.br>
2023-11-09 15:14:38 -0300

    add Exercise1

---
+++ src/Exercicios_BJL_Seq/Exercise1.java
@@ -0,0 +1,18 @@
+package Exercicios_BJL_Seq;
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter four numbers in sequence:");
+        int numberOne = input.nextInt();
+        int numberTwo = input.nextInt();
+        int numberThree = input.nextInt();
+        int numberFour = input.nextInt();
+
+        int sum = numberOne + numberTwo + numberThree + numberFour;
+
+        System.out.println("sum result: " + sum);
+    }
+}
