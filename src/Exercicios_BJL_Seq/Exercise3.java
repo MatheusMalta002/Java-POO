e2d51c21838f21d57f24007d97fdd9dffaf4d90e
Matheus Fillipe <mfsm@cin.ufpe.br>
2023-11-09 15:49:57 -0300

    add Exercise3

---
+++ src/Exercicios_BJL_Seq/Exercise3.java
@@ -0,0 +1,16 @@
+package Exercicios_BJL_Seq;
+import java.util.Scanner;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter salary:");
+        double salary = input.nextDouble();
+        double increase = salary * 0.25;
+
+        double newValue = salary + increase;
+
+        System.out.printf("The new salary is: %.2f%n", newValue);
+    }
+}
